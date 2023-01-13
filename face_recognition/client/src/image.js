aeaa3cf56d1880521893d1ac51850c7b84fc3515
nitish reddy <nitish.200327@gmail.com>
2023-01-13 22:35:29 +0530

    first commit

---
+++ face_recognition/client/src/image.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import img1 from './images/face.jpg';
+
+
+function Imagess(){
+    return(
+        <div>
+               <img src={img1}alt=""/>
+        </div>
+    )
+}
+
+export default Imagess;
